eefade42cbdf920875b426bb1f9d96a919ea807d
Mathieu Triay <mathieu@nightzookeeper.com>
2013-09-09 00:38:06 +0100

    Fix path format

--- test/test.js
+++ test/test.js
@@ -9,7 +9,7 @@ describe('S3', function () {
 
 	it('should list files in buckets with less than 1000 objects and use Prefix to filter', function (done) {
 
-		s3.listObjects({Prefix: '/sea', Bucket: __dirname + '/local/otters'}, function (err, data) {
+		s3.listObjects({Prefix: 'sea', Bucket: __dirname + '/local/otters'}, function (err, data) {
 
 			expect(err).to.equal(null);
 			expect(data.Contents.length).to.equal(560);
@@ -24,7 +24,7 @@ describe('S3', function () {
 
 	it('should list files in buckets with less than 1000 objects and use Prefix to filter', function (done) {
 
-		s3.listObjects({Prefix: '/river', Bucket: __dirname + '/local/otters'}, function (err, data) {
+		s3.listObjects({Prefix: 'river', Bucket: __dirname + '/local/otters'}, function (err, data) {
 
 			expect(err).to.equal(null);
 			expect(data.Contents.length).to.equal(912);
@@ -102,7 +102,7 @@ describe('S3', function () {
 			expect(fs.existsSync(__dirname + '/local/otters/sea/yo copy 4.txt')).to.equal(false);
 			expect(fs.existsSync(__dirname + '/local/otters/sea/yo copy 5.txt')).to.equal(false);
 
-			s3.listObjects({Prefix: '/sea', Bucket: __dirname + '/local/otters'}, function (err, data) {
+			s3.listObjects({Prefix: 'sea', Bucket: __dirname + '/local/otters'}, function (err, data) {
 
 				expect(err).to.equal(null);
 				expect(data.Contents.length).to.equal(556);
@@ -124,10 +124,10 @@ describe('S3', function () {
 		expect(fs.existsSync(__dirname + '/local/otters/sea/yo copy 8.txt')).to.equal(true);
 
 		var keys = [
-			{Key: '/sea/yo copy 20000.txt'}, 
-			{Key: '/sea/yo copy 6.txt'}, 
-			{Key: '/sea/yo copy 7.txt'}, 
-			{Key: '/sea/yo copy 8.txt'}
+			{Key: 'sea/yo copy 20000.txt'}, 
+			{Key: 'sea/yo copy 6.txt'}, 
+			{Key: 'sea/yo copy 7.txt'}, 
+			{Key: 'sea/yo copy 8.txt'}
 		];
 
 		var to_delete = {
@@ -144,7 +144,7 @@ describe('S3', function () {
 			expect(data.Deleted).to.exist;
 			expect(data.Errors.length).to.equal(1);
 			expect(data.Deleted.length).to.equal(3);
-			expect(data.Errors[0].Key).to.equal('/sea/yo copy 20000.txt');
+			expect(data.Errors[0].Key).to.equal('sea/yo copy 20000.txt');
 
 			expect(fs.existsSync(__dirname + '/local/otters/sea/yo copy 20000.txt')).to.equal(false);
 			expect(fs.existsSync(__dirname + '/local/otters/sea/yo copy 6.txt')).to.equal(false);
@@ -157,22 +157,22 @@ describe('S3', function () {
 
 	it('should get a file', function (done) {
 
-		s3.getObject({Key: '/sea/yo copy 10.txt', Bucket: __dirname + '/local/otters'}, function (err, data) {
+		s3.getObject({Key: 'sea/yo copy 10.txt', Bucket: __dirname + '/local/otters'}, function (err, data) {
 
 			expect(err).to.be.null;
 			expect(data.ETag).to.equal('"d41d8cd98f00b204e9800998ecf8427e"')
-			expect(data.Key).to.equal('/sea/yo copy 10.txt');
+			expect(data.Key).to.equal('sea/yo copy 10.txt');
 			done();
 		});
 	});
 
 	it('should get a file and its content', function (done) {
 
-		s3.getObject({Key: '/animal.txt', Bucket: __dirname + '/local/otters'}, function (err, data) {
+		s3.getObject({Key: 'animal.txt', Bucket: __dirname + '/local/otters'}, function (err, data) {
 			
 			expect(err).to.be.null;
 			expect(data.ETag).to.equal('"485737f20ae6c0c3e51f68dd9b93b4e9"')
-			expect(data.Key).to.equal('/animal.txt');
+			expect(data.Key).to.equal('animal.txt');
 			expect(data.Body.toString()).to.equal("My favourite animal");
 			done();
 		});
@@ -180,15 +180,15 @@ describe('S3', function () {
 
 	it('should create a file and have the same content in sub dir', function (done) {
 
-		s3.putObject({Key: '/punk/file', Body: fs.readFileSync(__dirname + '/local/file'), Bucket: __dirname + '/local/otters'}, function (err, data) {
+		s3.putObject({Key: 'punk/file', Body: fs.readFileSync(__dirname + '/local/file'), Bucket: __dirname + '/local/otters'}, function (err, data) {
 			
 			expect(err).to.be.null;
 			expect(fs.existsSync(__dirname + '/local/otters/punk/file')).to.equal(true);
 
-			s3.getObject({Key: '/punk/file', Bucket: __dirname + '/local/otters'}, function (err, data) {
+			s3.getObject({Key: 'punk/file', Bucket: __dirname + '/local/otters'}, function (err, data) {
 				
 				expect(err).to.be.null;
-				expect(data.Key).to.equal('/punk/file');
+				expect(data.Key).to.equal('punk/file');
 				expect(data.Body.toString()).to.equal("this is a file. That's right.");
 				done();
 			});
